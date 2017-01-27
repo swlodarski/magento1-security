61c69c7b49f6e859526241911b7e092e2ef1053e
Robert Brodie <robert@sumoheavy.com>
2017-01-27 18:09:04 -0500

    Shell script will now run from anywhere

--- shell/sumoheavy/security/scan.php
+++ shell/sumoheavy/security/scan.php
@@ -1,14 +1,19 @@
 <?php
-require_once '../../abstract.php';
+require_once dirname(dirname(dirname(__FILE__))) . DIRECTORY_SEPARATOR . 'abstract.php';
 
 class SUMOHeavy_Shell_Security_Scan extends Mage_Shell_Abstract
 {
     public function run()
     {
-        $cmsBlock = Mage::getModel('cms/block')->load($this->getArg('block_id'));
-
         $scanner = Mage::getModel('sumoheavy_security/rule');
-        $scanner->scanCmsBlock($cmsBlock);
+
+        if ($this->getArg('block_id')) {
+            $cmsBlock = Mage::getModel('cms/block')->load($this->getArg('block_id'));
+
+            $scanner->scanCmsBlock($cmsBlock);
+        } else {
+            $scanner->scanAllCmsBlocks();
+        }
     }
 }
 
