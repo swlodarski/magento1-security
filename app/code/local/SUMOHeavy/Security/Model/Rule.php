d16fa207ef3cd42edd235004f1ea9a8ac0121a66
Robert Brodie <robert@sumoheavy.com>
2017-01-27 18:13:52 -0500

    Added ability to scan all blocks

--- app/code/local/SUMOHeavy/Security/Model/Rule.php
+++ app/code/local/SUMOHeavy/Security/Model/Rule.php
@@ -33,6 +33,17 @@ class SUMOHeavy_Security_Model_Rule extends Mage_Core_Model_Abstract
     }
 
     /**
+     * CMS Block walker callback for scanner
+     *
+     * @param $args
+     */
+    public function cmsBlockCallback($args)
+    {
+
+        $this->scanCmsBlock($args['row']);
+    }
+
+    /**
      * Scans a single CMS Block for all matching rules
      *
      * @param Mage_Cms_Model_Block $cmsBlock
@@ -45,6 +56,19 @@ class SUMOHeavy_Security_Model_Rule extends Mage_Core_Model_Abstract
             $ruleCollection->getSelect(),
             array(array($this, 'ruleCallback')),
             array('cms_block' => $cmsBlock)
+        );
+    }
+
+    /**
+     * Scans all available CMS Blocks for matching rules
+     */
+    public function scanAllCmsBlocks()
+    {
+        $cmsBlockCollection = Mage::getModel('cms/block')->getCollection();
+
+        Mage::getSingleton('core/resource_iterator')->walk(
+            $cmsBlockCollection->getSelect(),
+            array(array($this, 'cmsBlockCallback'))
         );
     }
 }
